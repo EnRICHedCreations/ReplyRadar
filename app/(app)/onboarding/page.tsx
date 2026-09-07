e84ba5a2cde03ccc35b196c58ffc43aaa3089265
EnRICHedCreations <enrichedcreations96@gmail.com>
2026-09-07 08:49:58 -0400

    Use simplified onboarding radar editor

--- app/(app)/onboarding/page.tsx
+++ app/(app)/onboarding/page.tsx
@@ -1,4 +1,2 @@
-import { RadarForm } from "@/components/app-ui";
-export default function Page() {
-  return <RadarForm onboarding />;
-}
+import RadarEditor from "@/components/radar-editor";
+export default function Page(){return <RadarEditor/>;}
