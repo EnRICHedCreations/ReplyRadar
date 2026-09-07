aba38543d10798455c5912795cd02a42f12f38e4
EnRICHedCreations <enrichedcreations96@gmail.com>
2026-09-07 08:54:49 -0400

    Use simplified settings UI

--- app/(app)/settings/page.tsx
+++ app/(app)/settings/page.tsx
@@ -1,4 +1,2 @@
-import { Account } from "@/components/app-ui";
-export default function Page() {
-  return <Account preferences />;
-}
+import ProfileSettings from "@/components/profile-settings";
+export default function Page(){return <ProfileSettings/>;}
